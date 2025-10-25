ff6c247452512890575fd5d8fe6da6b4d41afebb
Feda <feeeda2003@gmsil.com>
2025-10-25 13:48:59 +0300

    Initial Commit

---
+++ plantreminder/plantreminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantreminderApp.swift
+//  plantreminder
+//
+//  Created by Feda  on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct plantreminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantreminderTests/plantreminderTests.swift
@@ -0,0 +1,16 @@
+//
+//  plantreminderTests.swift
+//  plantreminderTests
+//
+//  Created by Feda  on 25/10/2025.
+//
+
+import Testing
+
+struct plantreminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
